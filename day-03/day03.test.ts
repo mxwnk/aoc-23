8b7d98ed37a2c3a0b3fac9630098ecc7be6ea124
mxwnk <mx.wink@gmail.com>
2023-12-03 22:50:03 +0100

    Solve day3 - task 2

--- day-03/day03.test.ts
+++ day-03/day03.test.ts
@@ -1,4 +1,4 @@
-import { solveTask1} from "./day03";
+import { solveTask1, solveTask2 } from "./day03";
 
 describe("Day 3", () => {
     it("should solve task 1 with example data", () => {
@@ -6,9 +6,18 @@ describe("Day 3", () => {
         expect(result).toBe(4361);
     });
 
-    
     it("should solve task 1", () => {
         const result = solveTask1("input.txt");
+        console.log(result);
+    });
+    
+    it("should solve task 2 with example data", () => {
+        const result = solveTask2("example.txt");
+        expect(result).toBe(467835);
+    });
+    
+    it("should solve task 2 with real data", () => {
+        const result = solveTask2("input.txt");
         console.log(result);
     });
 });
--- day-03/day03.ts
+++ day-03/day03.ts
@@ -1,6 +1,62 @@
 import * as fs from "fs";
 
 const sum = (a: number, b: number): number => a + b;
+const isDigit = (char: string) => !isNaN(parseInt(char));
+
+export function solveTask2(inputFilePath: string) {
+    const engineSchematic = fs.readFileSync(`./day-03/${inputFilePath}`, { encoding: "utf8" });
+    const matrix: string[][] = engineSchematic.split("\n").map(rows => rows.split(""));
+    const gears: number[] = [];
+    for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+        const row = matrix[rowIndex];
+        for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+            const char = row[columnIndex];
+            if (char !== "*") {
+                continue;
+            }
+            const above = getPartNumbersInRange(matrix, rowIndex - 1, columnIndex - 1, 3)
+            const left = getPartNumbersInRange(matrix, rowIndex, columnIndex - 1, 1)
+            const right = getPartNumbersInRange(matrix, rowIndex, columnIndex + 1, 1)
+            const below = getPartNumbersInRange(matrix, rowIndex + 1, columnIndex - 1, 3)
+            const findings = [...above, ...left, ...right, ...below].filter(x => x !== undefined);
+            if (findings.length != 2) {
+                continue;
+            }
+            gears.push(parseInt(findings[0] as string) * parseInt(findings[1] as string));
+        }
+    }
+    return gears.reduce(sum);
+}
+
+function getPartNumbersInRange(matrix: string[][], rowIndex: number, columnIndex: number, searchLength: number) {
+    const partNumbers: string[] = [];
+    const row = matrix[rowIndex];
+    for (let index = columnIndex; index < searchLength + columnIndex; index++) {
+        try {
+            const char = row[index];
+            let partNumber = "";
+            if (!isDigit(char)) {
+                continue;
+            }
+            partNumber = char;
+            let left = index - 1;
+
+            while (isDigit(row[left])) {
+                partNumber = row[left] + partNumber;
+                left--;
+            }
+
+            index++;
+            while (isDigit(row[index])) {
+                partNumber = partNumber + row[index];
+                index++;
+            }
+            partNumbers.push(partNumber);
+
+        } catch { }
+    }
+    return partNumbers;
+}
 
 export function solveTask1(inputFilePath: string) {
     const engineSchematic = fs.readFileSync(`./day-03/${inputFilePath}`, { encoding: "utf8" });
