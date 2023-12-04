4fc59e41243b2b51284717b0dfad66848e3dd60f
mxwnk <mx.wink@gmail.com>
2023-12-04 13:04:06 +0100

    Solve day4 - task 2

--- day-04/day04.test.ts
+++ day-04/day04.test.ts
@@ -1,4 +1,4 @@
-import { solveTask1 } from "./day04";
+import { solveTask1, solveTask2 } from "./day04";
 
 describe("Day 4", () => {
     it("should solve task 1 with example data", () => {
@@ -6,8 +6,18 @@ describe("Day 4", () => {
         expect(result).toBe(13);
     });
 
-    it("should solve task 1", () => {
+    it("should solve task 1 with real data", () => {
         const result = solveTask1("input.txt");
+        console.log(`Result: ${result}`);
+    });
+    
+    it("should solve task 2 with example data", () => {
+        const result = solveTask2("example.txt");
+        expect(result).toBe(30);
+    });
+    
+    it("should solve task 2 with real data", () => {
+        const result = solveTask2("input.txt");
         console.log(`Result: ${result}`);
     });
 });
--- day-04/day04.ts
+++ day-04/day04.ts
@@ -7,6 +7,39 @@ export function solveTask1(inputFilePath: string) {
     return gameCards.map(calculateWinningPoints).reduce(sum);
 }
 
+export function solveTask2(inputFilePath: string) {
+    const input = fs.readFileSync(`./day-04/${inputFilePath}`, { encoding: "utf8" });
+    const gameCards = input.split("\n");
+    const result: Record<number, number> = {};
+    for (let index = 0; index < gameCards.length; index++) {
+        result[index + 1] = 1;
+    }
+    for (let index = 0; index < gameCards.length - 1; index++) {
+        const gameCard = gameCards[index];
+        const wins = calculateWins(gameCard);
+        for (let i = 1; i <= wins; i++) {
+            if (isNaN(result[index + i + 1])) {
+                break;
+            }
+            result[index + i + 1] += result[index + 1];
+        }
+    }
+
+    return Object.keys(result).map(k => result[k]).reduce(sum);
+}
+
+function calculateWins(gameCard: string) {
+    const parts = gameCard.split(":");
+    const gamePart = parts[1].split("|")
+    const winningNumbers = gamePart[0].split(" ").filter(isDigit);
+    const myNumbers = gamePart[1].split(" ").filter(isDigit);
+    const hits = myNumbers.filter(mn => winningNumbers.includes(mn)).length;
+    if (hits == 0) {
+        return 0;
+    }
+    return hits;
+}
+
 function calculateWinningPoints(gameCard: string) {
     const parts = gameCard.split(":");
     const gamePart = parts[1].split("|")
