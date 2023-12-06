603514440f424169b639a15f09dbda3ed19c920e
mxwnk <mx.wink@gmail.com>
2023-12-06 23:09:17 +0100

    Refactor solution calculation

--- day-06/day06.ts
+++ day-06/day06.ts
@@ -6,10 +6,13 @@ export function solveTask1(input: string) {
     .split("\n");
   const times = lines[0].split(" ").filter(isDigit).map(Number);
   const distances = lines[1].split(" ").filter(isDigit).map(Number);
-  const races: Race[] = times.map((t, i) => ({ time: t, distance: distances[i] }));
+  const races: Race[] = times.map((t, i) => ({
+    time: t,
+    distance: distances[i],
+  }));
 
   const possibleSolutions = Array(races.length).fill(0);
-  for (let index = 0; index < races.length; index ++) {
+  for (let index = 0; index < races.length; index++) {
     const race = races[index];
     const solutions = calculateSolutionsFor(race);
     possibleSolutions[index] = solutions;
@@ -27,18 +30,14 @@ export function solveTask2(input: string) {
   const distance = parseInt(
     lines[1].split(" ").filter(isDigit).reduce((a, b) => a + b),
   );
-  return calculateSolutionsFor({time, distance});
+  return calculateSolutionsFor({ time, distance });
 }
 
-type Race = {time: number, distance: number};
+type Race = { time: number; distance: number };
 
-function calculateSolutionsFor(race: Race){
-  let possibleSolutions = 0;
-  for (let index = 0; index < race.time; index++) {
-    const expectedDistance = (race.time - index) * index;
-    if (expectedDistance > race.distance) {
-      possibleSolutions++;
-    }
-  }
-  return possibleSolutions;
+function calculateSolutionsFor(race: Race) {
+  return [...Array(race.time).keys()]
+    .map((t) => (race.time - t) * t)
+    .filter((x) => x > race.distance)
+    .length;
 }
