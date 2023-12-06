6cacac17c1a1d570eadd77b77570438871f27c9f
mxwnk <mx.wink@gmail.com>
2023-12-06 23:09:17 +0100

    Refactor, merge race calculation into one function

--- day-06/day06.test.ts
+++ day-06/day06.test.ts
@@ -1,13 +1,23 @@
 import { solveTask1, solveTask2 } from "./day06";
 
 describe("Day 06", () => {
-  // it("should solve task 1 with example data", () => {
-  //   const result = solveTask1("example.txt");
-  //   expect(result).toBe(288);
-  // });
+  it("should solve task 1 with example data", () => {
+    const result = solveTask1("example.txt");
+    expect(result).toBe(288);
+  });
 
   it("should solve task 1 with real data", () => {
     const result = solveTask1("input.txt");
+    console.log(result);
+  });
+  
+  it("should solve task 2 with example data", () => {
+    const result = solveTask2("example.txt");
+    expect(result).toBe(71503);
+  });
+  
+  it("should solve task 2 with real data", () => {
+    const result = solveTask2("input.txt");
     console.log(result);
   });
 });
--- day-06/day06.ts
+++ day-06/day06.ts
@@ -2,26 +2,43 @@ import * as fs from "fs";
 import { isDigit } from "../utils";
 
 export function solveTask1(input: string) {
-  const lines = fs.readFileSync(`./day-06/${input}`, { encoding: "utf8" }).split("\n");
+  const lines = fs.readFileSync(`./day-06/${input}`, { encoding: "utf8" })
+    .split("\n");
   const times = lines[0].split(" ").filter(isDigit).map(Number);
   const distances = lines[1].split(" ").filter(isDigit).map(Number);
-  const races = times.map((t, i) => ({ time: t, distance: distances[i] }));
+  const races: Race[] = times.map((t, i) => ({ time: t, distance: distances[i] }));
 
   const possibleSolutions = Array(races.length).fill(0);
-  for (let rIndex = 0; rIndex < races.length; rIndex++) {
-    const race = races[rIndex];
-    for (let tIndex = 0; tIndex < race.time; tIndex++) {
-      const expectedDistance = (race.time - tIndex) * tIndex;
-      if(expectedDistance > race.distance){
-        possibleSolutions[rIndex]++;
-      }
-    }
-    
+  for (let index = 0; index < races.length; index ++) {
+    const race = races[index];
+    const solutions = calculateSolutionsFor(race);
+    possibleSolutions[index] = solutions;
   }
 
   return possibleSolutions.reduce((a, b) => a * b);
 }
 
 export function solveTask2(input: string) {
-  return 0;
+  const lines = fs.readFileSync(`./day-06/${input}`, { encoding: "utf8" })
+    .split("\n");
+  const time = parseInt(
+    lines[0].split(" ").filter(isDigit).reduce((a, b) => a + b),
+  );
+  const distance = parseInt(
+    lines[1].split(" ").filter(isDigit).reduce((a, b) => a + b),
+  );
+  return calculateSolutionsFor({time, distance});
+}
+
+type Race = {time: number, distance: number};
+
+function calculateSolutionsFor(race: Race){
+  let possibleSolutions = 0;
+  for (let index = 0; index < race.time; index++) {
+    const expectedDistance = (race.time - index) * index;
+    if (expectedDistance > race.distance) {
+      possibleSolutions++;
+    }
+  }
+  return possibleSolutions;
 }
