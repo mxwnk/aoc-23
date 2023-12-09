4c5c820134253d2f986d62c621c598ff5c9716b0
mxwnk <mx.wink@gmail.com>
2023-12-09 10:29:24 +0100

    Solve day9 - task 1

---
+++ day-09/day09.test.ts
@@ -0,0 +1,23 @@
+import { solveTask1, solveTask2 } from "./day09";
+
+describe("Day 09", () => {
+  it("should solve task 1 with example data", () => {
+    const result = solveTask1("example.txt");
+    expect(result).toBe(114);
+  });
+
+  it("should solve task 1 with real data", () => {
+    const result = solveTask1("input.txt");
+    console.log(result);
+  });
+
+  // it("should solve task 2 with example data", () => {
+  //   const result = solveTask2("example.txt");
+  //   expect(result).toBe(0);
+  // });
+
+  // it("should solve task 2 with real data", () => {
+  //   const result = solveTask2("input.txt");
+  //   console.log(result);
+  // });
+});
---
+++ day-09/day09.ts
@@ -0,0 +1,39 @@
+import * as fs from "fs";
+import { sum } from "../utils";
+
+export function solveTask1(input: string) {
+    const histories = fs.readFileSync(`./day-09/${input}`, { encoding: "utf8" }).split("\n").map(l => l.split(" ").map(Number));
+    const extrapolatians: number[] = [];
+    for (const history of histories) {
+        const iterations = [history];
+        let depth = 1;
+        while (!allZero(iterations[depth - 1])) {
+            for (let i = 0; i < iterations[depth - 1].length - 1; i++) {
+                if (iterations[depth] === undefined) {
+                    iterations[depth] = [];
+                }
+                iterations[depth].push(iterations[depth - 1][i + 1] - iterations[depth - 1][i])
+            }
+            depth++;
+        }
+        console.log(iterations);
+        for (let i = iterations.length - 1; 0 < i; i--) {
+            const currentIteration = iterations[i];
+            const nextIteration = iterations[i - 1];
+            const lastOfCurrent = currentIteration[currentIteration.length - 1];
+            const lastOfNextIteration = nextIteration[nextIteration.length - 1];
+            nextIteration.push(lastOfNextIteration + lastOfCurrent)
+        }
+
+        const firstIteration = iterations[0];
+        const extrapolatian = firstIteration[firstIteration.length - 1];
+        extrapolatians.push(extrapolatian)
+    }
+    return extrapolatians.reduce(sum);
+}
+
+const allZero = (array: number[]) => array.every(x => x === 0);
+
+export function solveTask2(input: string) {
+    return 0;
+}
