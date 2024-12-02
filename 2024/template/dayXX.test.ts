14c4d29b5bb52ec603089accce3499354d025ea3
mxwnk <mx.wink@gmail.com>
2024-12-02 14:43:56 +0100

    Use better logging for test template

--- 2024/template/dayXX.test.ts
+++ 2024/template/dayXX.test.ts
@@ -1,18 +1,21 @@
 import { solvePart1 , solvePart2 } from "./dayXX.ts";
 import { assertEquals } from "../utils/assert.ts";
+import { log } from "../infrastructure/logger.ts";
 
 export function part1Test(){
     const exampleResult = solvePart1('example');
     assertEquals(exampleResult, 1);
+    log.s(`Day XX - part 1 sample: ✅`);
     
     const actualResult = solvePart1('input');
-    console.log(`Day XX - part 1 result: '${actualResult}'`);
+    log.s(`Day XX - part 1 result: '${actualResult}'`);
 }
 
 export function part2Test(){
     const exampleResult = solvePart2('example');
     assertEquals(exampleResult, 1);
+    log.s(`Day XX - part 2 sample: ✅`);
 
     const actualResult = solvePart2('input');
-    console.log(`Day XX - part 2 result: '${actualResult}'`);
+    log.s(`Day XX - part 2 result: '${actualResult}'`);
 }
